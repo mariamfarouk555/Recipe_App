53b64bd269d476a1249df0984d7db07aa14c7353
mariam farouk <164365682+mariamfarouk555@users.noreply.github.com>
2026-04-15 19:07:21 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/Splach/Splachscreen.dart';
+import 'package:provider/provider.dart';
+import 'Splach/Splachscreen.dart';
+import 'home/viewModel/home_viewModel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Splachscreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Splachscreen(),
+      ),
+    );
   }
 }
